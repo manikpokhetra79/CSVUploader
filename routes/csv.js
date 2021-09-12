094ef910f3d1c5b418885aaf4f60e1b9aaeae86f
Manik Gupta <manikpokhetra@gmail.com>
2021-09-12 19:39:17 +0530

    created dummy api for upload action

--- routes/csv.js
+++ routes/csv.js
@@ -9,4 +9,7 @@ router.get('/displayfiles', csvController.displayfiles);
 router.post('/upload', csvController.upload);
 router.get('/delete/:id', csvController.deleteFile);
 router.get('/displaydata/:id', csvController.displayData);
+
+// dummy api route
+router.post('/api/upload', csvController.uploadViaApi);
 module.exports = router;
